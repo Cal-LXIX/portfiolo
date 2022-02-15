0045221b80931a2160950d21552aeba4e5904caf
Jean-Guy <lyoncal69@hotmail.fr>
2022-02-15 15:47:49 +0100

    update img

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
+/*La déclaration const permet de créer une constante nommée accessible uniquement en lecture*/
 const ratio = 0.2;
 const options = {
   root: null,
   rootMargin: "0px",
   threshold: ratio,
 };
-
+/* création fonction constante  */
 const handleIntersection = function (entries, observer) {
   entries.forEach(function (entry) {
     if (entry.intersectionRatio > ratio) {
+      /*Pour ajoutez une classe reveal-visible à un élément*/
       entry.target.classList.add("reveal-visible");
       observer.unobserve(entry.target);
     }
